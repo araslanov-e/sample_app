f94111ebb93ddcbf051b5123e285c03174a65529
araslanov.e.i <araslanov.e.i@createdigital.me>
2012-04-04 11:21:50 +0600

    contriller User, path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 SampleApp::Application.routes.draw do
-  get "pages/about"
 
-  get "pages/home"
+  get "users/new"
+  match '/signup', :to => 'users#new'
 
-  get "pages/contact"
+  match '/about', :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+
+  #get "pages/help"
+  #get "pages/about"
+  #get "pages/home"
+  #get "pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +61,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
