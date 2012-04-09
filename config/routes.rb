7b2f0cdc70f96079ee39622aa315d473c5993917
araslanov.e.i <araslanov.e.i@createdigital.me>
2012-04-09 11:19:40 +0600

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 SampleApp::Application.routes.draw do
 
+  #get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   #get "users/new"
 
-  match '/signup', :to => 'users#new'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/about', :to => 'pages#about'
   match '/contact', :to => 'pages#contact'
